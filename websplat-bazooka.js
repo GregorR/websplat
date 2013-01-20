5a475f5554435eeab40044b5126777d4d321f485
Gregor Richards <Richards@codu.org>
2013-01-20 18:26:05 -0500

    The bazooka now knocks sprites around a bit.
    --HG--
    branch : websplat2

--- websplat-bazooka.js
+++ websplat-bazooka.js
@@ -42,8 +42,18 @@
                 if (els === null) continue;
                 for (var i = 0; i < els.length; i++) {
                     var el = els[i];
-                    if ("wpSprite" in el) continue;
-                    if (WebSplat.elInDistance(el, bazRad, bazX, bazY)) {
+                    if ("wpSprite" in el) {
+                        // give it momentum
+                        var sprite = el.wpSprite;
+                        var dx = sprite.x - bazX;
+                        var dy = sprite.y - bazY;
+                        var dist = Math.sqrt(Math.pow(dx, 2) + Math.pow(dy, 2));
+                        if (dist < bazRad) {
+                            var angle = Math.atan((sprite.y - bazY) / (sprite.x - bazX));
+                            sprite.xvel = Math.cos(angle) * (bazRad - dist) * ((dx>0)?1:-1);
+                            sprite.yvel = Math.sin(angle) * (bazRad - dist) * ((dy>0)?1:-1);
+                        }
+                    } else if (WebSplat.elInDistance(el, bazRad, bazX, bazY)) {
                         WebSplat.remElementPosition(el);
                         el.style.visibility = "hidden";
                     }
