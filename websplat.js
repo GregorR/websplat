a9d68df7ca8139a061003cab2764b35dbd725470
Gregor Richards <Richards@codu.org>
2013-01-20 18:12:32 -0500

    Updated copyright headers.
    --HG--
    branch : websplat2

--- websplat.js
+++ websplat.js
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2010, 2012 Gregor Richards
+ * Copyright (c) 2010, 2012-2013 Gregor Richards
  *
  * Permission to use, copy, modify, and/or distribute this software for any
  * purpose with or without fee is hereby granted, provided that the above
